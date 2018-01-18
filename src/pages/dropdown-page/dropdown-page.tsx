00970c3a20c65c50191b8501d05a7e560a9651c4
AlexanderSergan <jfdi686@gmail.com>
2018-01-18 09:16:12 +0200

    inline styles fix

--- src/pages/dropdown-page/dropdown-page.tsx
+++ src/pages/dropdown-page/dropdown-page.tsx
@@ -50,29 +50,31 @@ export class StencilComponent {
                     <a class="dropdown-item" href="#">Separated link</a>
                 </div>
             </div>,
-            <div class="container" style="padding:100;">
-                <h2 style="margin-bottom:48px;">Dropdown component </h2>
+            <div class="container pt-4" >
+                <h2 class="mb-4">Dropdown component </h2>
                 <div class="row">
                     <div class="col-lg-12">
-                        <div class="jumbotron" style="padding-top:32px;">
+                        <div class="jumbotron pt-3" >
                             <h3>Usage </h3>
-                            <p style="font-size:22px;">To set position of the dropdown component you can pass position and align variables: </p>
+                            <p>To set position of the dropdown component you can pass position and align variables: </p>
+                            {/* <p style="font-size:22px;">To set position of the dropdown component you can pass position and align variables: </p> */}
                             <div class="row">
                                 <div class="col-lg-8">
-                                    <div class="dropdown show" style="margin-top:40px;margin-left:100px;margin-bottom:120px;"><button class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="true" type="button" style="margin-left:86px;margin-top:13px;">Example dropdown </button>
+                                    <div class="dropdown show mt-4 ml-5 mb-5">
+                                        <button class="btn btn-primary dropdown-toggle ml-5 mt-2" data-toggle="dropdown" aria-expanded="true" type="button" >Example dropdown </button>
                                         <div class="dropdown-menu show" role="menu"><a class="dropdown-item" role="presentation" href="#">First Item</a><a class="dropdown-item" role="presentation" href="#">Second Item</a><a class="dropdown-item" role="presentation" href="#">Third Item</a></div>
                                     </div>
                                 </div>
                                 <div class="col-lg-4">
-                                    <div class="dropdown"><button class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button" style="margin-left:2px;margin-top:13px;">Select position</button>
+                                    <div class="dropdown"><button class="btn btn-info dropdown-toggle ml-1 mt-2" data-toggle="dropdown" aria-expanded="false" type="button" >Select position</button>
                                         <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">Bottom </a><a class="dropdown-item" role="presentation" href="#">Left </a><a class="dropdown-item" role="presentation" href="#">Right </a><a class="dropdown-item" role="presentation"
                                             href="#">Top </a><a class="dropdown-item" role="presentation" href="#"> </a></div>
                                     </div>
-                                    <div class="dropdown"><button class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button" style="margin-left:3px;margin-top:13px;">Select alignment</button>
+                                    <div class="dropdown"><button class="btn btn-info dropdown-toggle ml-1 mt-2" data-toggle="dropdown" aria-expanded="false" type="button" >Select alignment</button>
                                         <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">None (centered)</a><a class="dropdown-item" role="presentation" href="#">Start </a><a class="dropdown-item" role="presentation" href="#">End </a><a class="dropdown-item"
                                             role="presentation" href="#"> </a></div>
                                     </div>
-                                    <div class="dropdown"><button class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button" style="margin-left:3px;margin-top:13px;">Button overflow</button>
+                                    <div class="dropdown"><button class="btn btn-info dropdown-toggle ml-1 mt-2" data-toggle="dropdown" aria-expanded="false" type="button" >Button overflow</button>
                                         <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">None (centered)</a><a class="dropdown-item" role="presentation" href="#">Start </a><a class="dropdown-item" role="presentation" href="#">End </a><a class="dropdown-item"
                                             role="presentation" href="#"> </a></div>
                                     </div>
